0116085253403a54f2c5f2d4364abe495de65b68
Manfred <Manfred@192.168.1.8>
2020-07-16 01:34:43 -0600

    Agregando validaciones "Practica"

--- web/JavaScript/Validations.js
+++ web/JavaScript/Validations.js
@@ -4,3 +4,17 @@
  * and open the template in the editor.
  */
 
+  function validaCedula() { 
+  var expreg = /^[0-9]{9}$/;
+  var m = document.getElementById("cedula").value;
+  var n = document.getElementById("cedula");
+  
+  if(expreg.test(m)){
+	alert("La cédula está bien digitada"); 
+        $(n).css("borderColor", "green");
+    }else {
+    alert("La cédula está mal digitada"); 
+    $(n).css("borderColor", "red");
+    }
+    
+  }
